6f9783d4e1266b2d10f8e8dc88e63b409e83f89c
Mark Stein <markstein820@gmail.com>
2015-10-07 19:46:11 -0400

    Main Version 0.01

---
+++ source/ScrollState.hx
@@ -0,0 +1,75 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxMath;
+import flixel.util.FlxSpriteUtil;
+import flixel.addons.display.FlxBackdrop;
+import flixel.text.FlxText;
+import source.Truck;
+
+/**
+ * ...
+ * @author ...
+ */
+class ScrollState extends FlxState
+{
+	public var backdrop:FlxBackdrop;
+	public var road:FlxBackdrop;
+	private var _player: Truck;
+	private var text: FlxText;
+	
+	//testing perposes
+	private var _testBTN : FlxButton;
+	/////////////////////////////////
+
+	override public function create():Void
+	{
+		super.create();
+		
+		backdrop = new FlxBackdrop("assets/images/backdrop.png");
+		backdrop.velocity.x = -200;
+		add(backdrop);
+		
+		road = new FlxBackdrop("assets/images/road.png", 0, 0, true, false);
+		road.y = 448;
+		road.velocity.x = -750;
+		add(road);
+		
+		_player = new Truck(100, 450);
+		_player.speed = 250;
+		add(_player);
+		
+		text = new FlxText(10, 10, 200, "Alcohol: " + _player.alcoholLevel, 14);
+		add(text);
+		
+		//testing perposes
+		_testBTN = new FlxButton(0,0,"Change", clickToChange);
+		add(_testBTN);
+		////////////////////////////////////////////////////////
+	}
+	
+	//testing perposes
+	private function clickToChange():Void 
+	{
+		FlxG.switchState(new TownState());
+		super.create();
+	}
+	///////////////////////////////////////
+	
+	override public function destroy():Void
+	{
+		super.destroy();
+	}
+	
+	override public function update():Void
+	{
+		super.update();
+		text.text = "Alcohol: " + _player.alcoholLevel;
+		FlxSpriteUtil.bound(_player, 0, FlxG.width, 448, FlxG.height);
+	}
+	
+}
